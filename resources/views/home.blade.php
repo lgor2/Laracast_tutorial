ab5d809226364cf759d8be2ec0bdb613794d2050
lgor2 <lgor.2.lgor.2.lgor@gmail.com>
2024-07-24 17:42:22 +0300

    Lesson 6. In progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,4 @@
     <x-slot:heading>
         Home page
     </x-slot:heading>
-
-    <h1>Hello from the home page!</h1>
 </x-layout>
---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-slot:heading>
+        Jobs page
+    </x-slot:heading>
+    <ul>
+        @foreach($jobs as $job)
+            <a href="/jobs/">
+                <li><strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year.</li>
+            </a>
+        @endforeach
+    </ul>
+</x-layout>
