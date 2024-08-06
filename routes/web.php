42c24ea740ca0a4f09f5ad0d3a0031101572fc95
lgor2 <lgor.2.lgor.2.lgor@gmail.com>
2024-08-06 13:29:50 +0300

    Lesson 18. Added edit and delete functions for a job.

--- routes/web.php
+++ routes/web.php
@@ -8,22 +8,24 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
     return view('jobs.index', ['jobs' => $jobs]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
-
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
-
+// Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -35,6 +37,39 @@ Route::post('/jobs', function () {
         'salary' => request('salary'),
         'employer_id' => 1,
     ]);
+
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title'=> request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    $job = Job::findOrFail( $id);
+    $job->delete();
 
     return redirect('/jobs');
 });
