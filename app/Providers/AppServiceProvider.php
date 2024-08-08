8ae409e9a38937afaf6c27b05a77e119477afb03
Igor <lgor.2.lgor.2.lgor@gmail.com>
2024-08-09 00:33:14 +0300

    Lesson 23. Created policies to control users rights to edit jobs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use App\Models\Job;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading();
+
+        // Gate::define('ediit-job', function (User $user, Job $job){
+        //     return $job->employer->user->is($user);
+        // });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,21 +9,33 @@ Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 // Route::controller((JobController::class))->group(function () {
-//     Route::get('/jobs', 'index');
-//     Route::get('/jobs/create', 'create');
-//     Route::get('/jobs/{job}', 'show');
-//     Route::post('/jobs', 'store');
-//     Route::get('/jobs/{job}/edit', 'edit');
-//     Route::patch('/jobs/{job}', 'update');
-//     Route::delete('/jobs/{job}', 'destroy');
+//     Route::get('/jobs', ['index']);
+//     Route::get('/jobs/create', ['create']);
+//     Route::get('/jobs/{job}', ['show']);
+//     Route::post('/jobs', ['store']);
+//     Route::get('/jobs/{job}/edit', ['edit']);
+//     Route::patch('/jobs/{job}', ['update']);
+//     Route::delete('/jobs/{job}', ['destroy']);
 // });
 
-Route::resource('jobs', JobController::class);
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
+
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
+    ->middleware("auth")
+    ->can('edit', 'job');
+    
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+
+// Route::resource('jobs', JobController::class)->middleware('auth');
 
 // Auth
 Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
