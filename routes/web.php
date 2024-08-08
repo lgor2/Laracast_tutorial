2cdc76c93d1a58e0e3a7e993368778fad826f782
Igor <lgor.2.lgor.2.lgor@gmail.com>
2024-08-08 15:54:54 +0300

    Lesson 21. Created frontEnd for registration and login form

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
@@ -17,3 +19,10 @@ Route::view('/contact', 'contact');
 // });
 
 Route::resource('jobs', JobController::class);
+
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
